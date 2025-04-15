df0e8cfe149a0dc627e956606df9f90c534280ce
Vednarayan Hiralkar <168326157+Ved3205@users.noreply.github.com>
2025-04-15 18:10:43 +0530

    Update conn.js

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://ved:ved@cluster0.qvn7q.mongodb.net/BookHive?retryWrites=true&w=majority&appName=Cluster0").then((res) => console.log("Connected"));
+mongoose.connect("mongodb+srv://<id>:<password>@cluster0.qvn7q.mongodb.net/BookHive?retryWrites=true&w=majority&appName=Cluster0").then((res) => console.log("Connected"));
